40325f7a59504991908966fcf09d67e2202c1854
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-05-31 15:02:19 -0700

    Add remaining Chess units/define background image

--- setup.js
+++ setup.js
@@ -33,7 +33,17 @@ module.exports = {
         var units = {};
         var i = 0;
 
-        // Set up light units
+        // Set up back line light units
+        units[i++] = new Rook(0, 0, 0, ":/chess/images/wRook.bmp");
+        units[i++] = new Knight(1 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wKnight.bmp");
+        units[i++] = new Bishop(2 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wBishop.bmp");
+        units[i++] = new King(3 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wKing.bmp");
+        units[i++] = new Queen(4 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wQueen.bmp");
+        units[i++] = new Bishop(5 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wBishop.bmp");
+        units[i++] = new Knight(6 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wKnight.bmp");
+        units[i++] = new Rook(7 * GRID_SIZE_SCALED, 0, 0, ":/chess/images/wRook.bmp");
+
+        // Set up front line light units
         for (var wp = 0; wp < 8; wp++)
             units[i++] = new Pawn(
                 wp * GRID_SIZE_SCALED,
@@ -42,7 +52,7 @@ module.exports = {
                 ":/chess/images/wPawn.bmp"
             );
 
-        // Set up dark units
+        // Set up front line dark units
         for (var bp = 0; bp < 8; bp++)
             units[i++] = new Pawn(
                 bp * GRID_SIZE_SCALED,
@@ -51,12 +61,24 @@ module.exports = {
                 ":/chess/images/bPawn.bmp"
             );
 
+        // Set up back line dark units
+        units[i++] = new Rook(0, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bRook.bmp");
+        units[i++] = new Knight(1 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bKnight.bmp");
+        units[i++] = new Bishop(2 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bBishop.bmp");
+        units[i++] = new King(3 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bKing.bmp");
+        units[i++] = new Queen(4 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bQueen.bmp");
+        units[i++] = new Bishop(5 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bBishop.bmp");
+        units[i++] = new Knight(6 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bKnight.bmp");
+        units[i++] = new Rook(7 * GRID_SIZE_SCALED, 7 * GRID_SIZE_SCALED, 0, ":/chess/images/bRook.bmp");
+
+
         return {
             bounds: {
                 x: 0, y: 0,
                 w: GRID_SIZE_SCALED * GRID_COLS,
                 h: GRID_SIZE_SCALED * GRID_ROWS
             },
+            background: ":/images/board.bmp",
             units: units
         };
     }
