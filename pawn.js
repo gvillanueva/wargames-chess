42d92df6216e5ee7a68051166f4bc1ba171bf114
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2016-05-30 22:50:08 -0700

    Add setup of chess pawns

--- pawn.js
+++ pawn.js
@@ -1,3 +1,28 @@
 /**
- * Created by cvillanueva on 5/17/2016.
+ * @file Defines the behavior of the Pawn in chess
+ * @author Giancarlo Villanueva
  */
+var WargamesUnit = require("../wargames/unit.js");
+var WargamesError = require("../wargames/error.js");
+
+Pawn.prototype = Object.create(WargamesUnit);
+Pawn.prototype.constructor = Pawn;
+
+function Pawn(x,y,z) {
+    WargamesUnit.prototype.constructor.call(this,x,y,z);
+}
+
+/**
+ *
+ * @param x X-coordinate of unit's destination.
+ * @param y Y-coordinate of unit's destination.
+ * @param z Z-coordinate of unit's destination.
+ */
+Pawn.prototype.move = function(x,y,z) {
+    WargamesUnit.prototype.move.call(this,x,y,z);
+    console.log("Pawn.move()");
+    if (x != this.x)
+        throw new WargamesError("Invalid move: Pawn only move diagonally when attacking");
+}
+
+module.exports = Pawn;
--- setup.js
+++ setup.js
@@ -1,14 +1,56 @@
 /**
- * Created by cvillanueva on 5/17/2016.
+ * @file Defines the extension points for checkers system
+ * @author Giancarlo Villanueva
  */
-var Unit = require("unit.js");
+const Pawn = require("./pawn.js");
+const Rook = require("./rook.js");
+const Knight = require("./knight.js");
+const Bishop = require("./bishop.js");
+const Queen = require("./queen.js");
+const King = require("./king.js");
 
-module.exports.setup = function() {
-    var units = {};
-    for(var i = 0; i < 8; i++)
-        units[i] = new Unit(0, i*8, 0);
+// Chessboard size constants
+const GRID_SIZE = 2.25;
+const GRID_ROWS = 8;
+const GRID_COLS = 8;
+const GRID_SCALE = 20;
+const GRID_SIZE_SCALED = GRID_SIZE * GRID_SCALE;
 
-    return units;
+module.exports = {
+    /**
+     *
+     * @param users
+     */
+    preStart: function(users) {
+    },
+
+    /**
+     * Sets up the units required for a game of checkers.
+     * @returns {Object} Collection of  Unit-derived objects
+     */
+    setup: function() {
+        var bounds = {};
+        var units = {};
+        var i = 0;
+
+        // Set up light units
+        for (var wp = 0; wp < 8; wp++)
+            units[i++] = new Pawn(
+                wp * GRID_SIZE_SCALED,
+                1 * GRID_SIZE_SCALED, 0);
+
+        // Set up dark units
+        for (var bp = 0; bp < 8; bp++)
+            units[i++] = new Pawn(
+                bp * GRID_SIZE_SCALED,
+                6 * GRID_SIZE_SCALED, 0);
+
+        return {
+            bounds: {
+                x: 0, y: 0,
+                w: GRID_SIZE_SCALED * GRID_COLS,
+                h: GRID_SIZE_SCALED * GRID_ROWS },
+            units: units
+        };
+    }
 }
-
-console.log("asdf");
